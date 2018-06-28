2d004e11a37f2934dd1f0c471ac9d5bfd183a5c5
Tony Su <peitaosu@163.com>
2018-06-28 21:08:41 +0800

    fix output issue and refine the arguments

--- split.py
+++ split.py
@@ -4,22 +4,29 @@ def ape_to_flac(ape, flac):
     os.system("ffmpeg -i {} {}".format(ape, flac))
 
 def gbk_to_utf8(cue):
-    cue_tmp = cue + ".tmp"
-    os.rename(cue, cue_tmp)
-    os.system("iconv -f gbk -t utf8 {} > {}".format(cue_tmp, cue))
-    os.remove(cue_tmp)
+    cue_bak = cue + ".bak"
+    os.rename(cue, cue_bak)
+    os.system("iconv -f gbk -t utf8 {} > {}".format(cue_bak, cue))
 
 def split(flac, cue, output, format="flac"):
+    if not os.path.isdir(output):
+        os.makedirs(output)
     if format == "mp3":
-        os.system("shntool split -t \"%n.%p.%t\" -f {} -o \"cust ext=mp3 lame -b 320 - %f\" {} -d {}".format(cue, flac, output))
+        os.system("shntool split -t \"%n.%p.%t\" -f {} -o \"cust ext=mp3 lame -b 320 - %f\" -d {} {}".format(cue, output, flac))
     else:
-        os.system("shntool split -t \"%n.%p.%t\" -f {} -o flac {} -d {}".format(cue, flac, output))
+        os.system("shntool split -t \"%n.%p.%t\" -f {} -o flac  -d {} {}".format(cue, output, flac))
 
 if __name__=="__main__":
-    ape = sys.argv[1]
-    cue = sys.argv[2]
-    flac = sys.argv[3]
-    output = sys.argv[4]
-    ape_to_flac(ape, flac)
-    gbk_to_utf8(cue)
-    split(flac, cue, output)
+    if len(sys.argv) < 3:
+        print "python split.py <in_dir> <out_dir>"
+        sys.exit()
+    in_dir = sys.argv[1]
+    out_dir = sys.argv[2]
+    cuefiles = [os.path.join(in_dir, f) for f in os.listdir(in_dir) if os.path.isfile(os.path.join(in_dir, f)) and f.endswith(".cue")]
+    for cue in cuefiles:
+        ape = cue.replace(".cue", ".ape")
+        flac = cue.replace(".cue", ".flac")
+        output = os.path.join(out_dir, os.path.basename(cue).replace(".cue", ""))
+        ape_to_flac(ape, flac)
+        gbk_to_utf8(cue)
+        split(flac, cue, output)
